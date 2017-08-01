c2129f1485565452fa0e268ed2a700c041029bc1
abc3660170 <3413218@qq.com>
2017-08-02 00:04:41 +0800

    模块化组件

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var fs = require('fs');
 var rename = require('./lib/rename')
+var resize = require('./lib/resize')
 var path = require('path')
 /******* 定义环境变量 *******/
 const uuidv1 = require('uuid/v1');
-var INPUT_DIR = 'C:\\Users\\Tao\\Documents\\WeChat Files\\ct890908\\CustomEmotions',
+var INPUT_DIR = 'C:\\Users\\Tao\\Documents\\WeChat Files\\ct890908\\test',
     OUTPUT_DIR = 'D:\\output',
     OUTPUT_DIR_TEMP = OUTPUT_DIR+ path.sep +uuidv1();
     OUTPUT_NAME = 'emoji';
@@ -12,9 +13,13 @@ var INPUT_DIR = 'C:\\Users\\Tao\\Documents\\WeChat Files\\ct890908\\CustomEmotio
 // 通过指定的路径查找表情文件
 fs.access(INPUT_DIR,function(err){
     if(!err){
-        rename(INPUT_DIR,OUTPUT_DIR_TEMP,{"name":OUTPUT_NAME})
-            .then(function(){
+        rename(INPUT_DIR,OUTPUT_DIR_TEMP,{"name":OUTPUT_NAME,"ext":"gif"})
+            .then(function(files){
                 console.log("微信改名OK")
+                resize(files,{"ext":"gif"})
+            })
+            .then(function(){
+                console.log("尺寸全部调整完成")
             })
             .catch(err2 => {
                 console.log("微信表情生成失败!",err2)
